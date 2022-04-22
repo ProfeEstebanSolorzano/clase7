e5d63c6da9b2a71804312fbf3124c33d0937d45a
EstebanSolorzano <chewva_k@hotmail.com>
2022-04-21 20:45:11 -0600

    formulario casi listo

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function procesarFormulario() {
+    const persona = {};
 
+    const listaInputs = document.querySelectorAll('input[type=text]');
+    //Esto:
+    // const objetoInput = listaInputs[0];
+    // const atributo = objetoInput['attributes'];
+    // const keyNombre = atributo.name;
+    // const nombre = keyNombre.value;
+    //es lo mismo que esto:
+    // listaInputs[0].attributes.name.value;
+
+    for (let index = 0; index < listaInputs.length; index++) {
+        const atributo = listaInputs[index].attributes.name.value
+        persona[atributo] = document.querySelector(`input[name=${atributo}]`).value;
+    }
+
+    console.log(!!persona.peso.match(/[A-Za-z]/));
     return false;
 }
