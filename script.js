1c0d8a4eed5022e120259cd71a9b76ec0ec9c093
EstebanSolorzano <chewva_k@hotmail.com>
2022-04-26 19:35:50 -0600

    tarea lista

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function procesarFormulario() {
     const persona = {};
 
     const listaInputs = document.querySelectorAll('input[type=text]');
+    const listaFinal = Array.from(listaInputs).concat(Array.from(document.querySelectorAll('input[type=radio]:checked')));
+
     //Esto:
     // const objetoInput = listaInputs[0];
     // const atributo = objetoInput['attributes'];
@@ -10,11 +12,20 @@ function procesarFormulario() {
     //es lo mismo que esto:
     // listaInputs[0].attributes.name.value;
 
-    for (let index = 0; index < listaInputs.length; index++) {
-        const atributo = listaInputs[index].attributes.name.value
-        persona[atributo] = document.querySelector(`input[name=${atributo}]`).value;
+    for (let index = 0; index < listaFinal.length; index++) {
+        const atributo = listaFinal[index].attributes.name.value
+        persona[atributo] = listaFinal[index].value;
+    }
+    //expresiones regulares
+    if (persona.altura.match(/[A-Za-z]/) || persona.peso.match(/[A-Za-z]/)) {
+        alert('Los campos altura y/o peso no pueden llevar letras');
+        return false;
     }
 
-    console.log(!!persona.peso.match(/[A-Za-z]/));
+    document.querySelector('#display').classList.remove('escondida');
+    document.querySelector('#display').classList.add('visible');
+    document.querySelector('#display').textContent = JSON.stringify(persona, null, 4);
+
+    //console.log(persona);
     return false;
 }
